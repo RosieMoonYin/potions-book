672024960f02b35c4d4d20b6a518e448f7b20ca4
Rosie MoonYin <rosie.victoria.burnett@gmail.com>
2024-12-12 13:35:14 +0100

    feat: strapi installation

---
+++ potions-backend/src/api/potion/controllers/potion.ts
@@ -0,0 +1,7 @@
+/**
+ * potion controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::potion.potion');
---
+++ potions-backend/src/api/potion/services/potion.ts
@@ -0,0 +1,7 @@
+/**
+ * potion service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::potion.potion');
